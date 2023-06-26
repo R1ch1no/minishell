8cb462c79a32f8e4cc6a011aab1ebd0140745bd9
R1ch1no <richard.kurnava@gmail.com>
2023-06-26 14:53:25 +0200

    exit fixed

--- src/exit.c
+++ src/exit.c
@@ -2,11 +2,29 @@
 #include "../minishell.h"
 #include <limits.h>
 
+int	overflow(long long result, char num, int sign, t_data *data)
+{
+	long long	res;
+
+	res = result * 10 * sign;
+	if (res == 9223372036854775800 && num == '8')
+	{
+		data->problem = 1;
+		return (1);
+	}
+	if (res == -9223372036854775800 && num == '9')
+	{
+		data->problem = 1;
+		return (1);
+	}
+	return (0);
+}
+
 long long	ft_atoi_exit(const char *str, t_data *data)
 {
-	int				i;
-	int				sign;
-	long long int	result;
+	int			i;
+	int			sign;
+	long long	result;
 
 	i = 0;
 	sign = 1;
@@ -21,16 +39,15 @@ long long	ft_atoi_exit(const char *str, t_data *data)
 	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		if (((result * 10 + (str[i] - '0') * sign) > LLONG_MAX || ((result * 10
-						+ (str[i] - '0') * sign) < LLONG_MIN)))
-			data->problem = 1;
+		if (overflow(result, str[i], sign, data) == 1)
+			return (sign * result);
 		result = result * 10 + (str[i] - '0');
 		i++;
 	}
 	return (sign * result);
 }
 
-int	is_num(char *argv)
+int	is_num(char *argv, t_data *data)
 {
 	int	i;
 
@@ -38,10 +55,30 @@ int	is_num(char *argv)
 	while (argv[++i])
 	{
 		if ((argv[i] < '0' || argv[i] > '9'))
+		{
 			if (argv[i] != '+' && argv[i] != '-')
+			{
+				data->problem = 1;
 				return (1);
+			}
+		}
 	}
 	return (0);
+}
+
+void	exit_for_exit(t_data *data, char ***args)
+{
+	free_2d_str_arr(args);
+	if (data->problem == 1)
+	{
+		cleanse(data);
+		exit(2);
+	}
+	else
+	{
+		cleanse(data);
+		exit(1);
+	}
 }
 
 int	ft_exit(t_data *data, char ***args)
@@ -51,15 +88,16 @@ int	ft_exit(t_data *data, char ***args)
 	if ((*args)[1] == NULL)
 	{
 		free_2d_str_arr(args);
+		cleanse(data);
 		exit(0);
 	}
 	if ((*args)[2] != NULL)
 	{
 		ft_putstr_fd("too many argumens\n", 2);
-		return (free_2d_str_arr(args), 0);
+		exit_for_exit(data, args);
 	}
 	num = ft_atoi_exit((*args)[1], data);
-	if (is_num((*args)[1]) == 0 && data->problem == 0)
+	if (is_num((*args)[1], data) == 0 && data->problem == 0)
 	{
 		cleanse(data);
 		ft_putstr_fd("exit\n", 1);
@@ -68,5 +106,6 @@ int	ft_exit(t_data *data, char ***args)
 	}
 	free_2d_str_arr(args);
 	ft_putstr_fd("numeric argument required\n", 2);
+	exit_for_exit(data, args);
 	return (0);
 }
